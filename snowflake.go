2edd68d761153c3ce61618cd63c5ff995d9b4f0f
andrew <andrewnijmeh1@gmail.com>
2023-02-19 22:28:42 -0500

    write some gen

--- snowflake.go
+++ snowflake.go
@@ -11,10 +11,30 @@ type Snowflake struct {
   lastSequenceExhaustion uint64
 }
 
-type DecodedSnowflake {
+type DecodedSnowflake struct {
   id uint64
   timestamp uint64
   nodeID uint32
   seq uint32
   epoch uint64
+}
+
+func NewSnowflakeWithNodeID(epoch uint64, nodeID uint32) *Snowflake {
+  return &Snowflake {
+    epoch: epoch,
+    nodeID: nodeID,
+    seq: 0,
+    lastSequenceExhaustion: 0,
+  }
+}
+
+func (sf *Snowflake) gen() string {
+  return sf.genWithTs(uint64(time.Now().Unix()))
+}
+ 
+func (sf *Snowflake) genWithTs(timestamp uint64) string {
+  if sf.seq >= 4095 && timestamp == sf.lastSequenceExhaustion {
+    for time.Now().Unix()-int64(timestamp) < 1 {
+    }
+  }
 }
